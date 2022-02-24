fd1beb039933fea928b85ba3424ccac8323294c8
flask3 <ay6859@gmail.com>
2022-02-24 15:49:42 +0800

    some more detections for ngCheck.py

--- ngCheck.py
+++ ngCheck.py
@@ -26,7 +26,6 @@ def ShangTouCheck(msg):
         current_point = 5
         reason = 3
     
-    #  ng = ['ㄋ', 'ㄍ']
     msg_first = getBopomofoFirst(msg)
     msg_raw = getBopomofo(msg)
     
@@ -35,16 +34,25 @@ def ShangTouCheck(msg):
     print(msg_first)
     print(msg_raw)
     
-    if BigCheck(msg_raw): 
+    if "黑鬼" in msg:
+        current_point = 20
+        reason = 10
+    elif HeiGuiCheck(msg_raw):
+        if current_point < 10:
+            current_point = 10
+            reason = 8
+    elif LightHeiGuiCheck(msg_raw):
+        if current_point < 5:
+            current_point = 5
+            reason = 9        
+    elif BigCheck(msg_raw): 
         if current_point < 10: 
             current_point = 10 
             reason = 4
-
     elif SmallCheck(msg_raw):
         if current_point < 5: 
             current_point = 5
             reason = 5
-
     elif TinyCheck(msg_first):
         if current_point < 2: 
             current_point = 2 
@@ -57,8 +65,41 @@ def ShangTouCheck(msg):
     print(current_point, reason)
     return current_point
 
+def HeiGuiCheck(list1):
+    if len(list1) < 2:
+        return False
+
+    # 爛演算法 但先這樣吧
+    mark = False
+    for t in list1:
+        if t == 'ㄏㄟ':
+            mark = True
+        elif t == 'ㄍㄨㄟˇ' and mark is True:
+            return True
+        elif mark is True and t != 'ㄍㄨㄟˇ':
+            mark = False
+    
+    return False
+
+def LightHeiGuiCheck(list1):
+    if len(list1) < 2:
+        return False
+
+    # 爛演算法 但先這樣吧
+    mark = False
+    for t in list1:
+        if 'ㄏㄟ' in t:
+            mark = True
+        elif 'ㄍㄨㄟ' in t and mark is True:
+            return True
+        elif mark is True and 'ㄍㄨㄟ' not in t:
+            mark = False
+    
+    return False
+
 def ZhuYinCheck(list1):
-    t = ['ㄋㄍ', 'ㄋㄧˊㄍㄜ', 'ㄋㄧㄍㄜ', 'ㄋㄍㄜ', 'ㄋㄧㄍ', 'ㄋㄧˊㄍ']
+    t = ['ㄋㄍ', 'ㄋㄧˊㄍㄜ', 'ㄋㄧㄍㄜ', 'ㄋㄍㄜ', 'ㄋㄧㄍ', 'ㄋㄧˊㄍ', 
+        'ㄏㄟㄍㄨㄟ', 'ㄏㄟㄍㄨㄟˇ', 'ㄏㄍ', 'ㄏㄟㄍ', 'ㄏㄍㄨㄟ', 'ㄏㄍㄨㄟˇ']
     
     return bool(set(t).intersection(list1)) #滿酷的寫法
 
@@ -114,7 +155,8 @@ def TinyCheck(list1):
 
 
 
-ShangTouCheck("ㄜ")
-ShangTouCheck("ㄋㄧˊ")
-ShangTouCheck("ㄋㄧˊㄍㄜ")
-ShangTouCheck("一個")
+ShangTouCheck("黑鬼")
+ShangTouCheck("黑軌")
+ShangTouCheck("嘿龜")
+ShangTouCheck("漢光")
+ShangTouCheck("ㄏㄍ")
